df1e796ee6c5836a3144d58ff69ec1af0cd3d2dd
arif1193 <rahman1193arif@gmail.com>
2021-10-19 02:22:01 +0600

    without prictice ja hoy

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 
 const Registration = () => {
+    const auth = getAuth();
+    const [userEmail, setUserEmail] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+    const handleRegistration = (e) => {
+        console.log(userEmail, userPassword)
+        e.preventDefault(userEmail, userPassword);
+    }
+
+    const handleEmailChange = e => {
+        setUserEmail(e.target.value)
+    }
+
+    const handlePassword = e => {
+        setUserPassword(e.target.value)
+    }
+
     return (
         <div>
-            <h2>Please register</h2>
-            <form>
-                <input type="Name" />
-                <br />
-                <input type="password" name="" id="" />
-                <br />
-                <input type="submit" value="" />
-            </form>
+            <h2 className="btn btn-success">Please register</h2>
+            <Form onSubmit={handleRegistration}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control onBlur={handleEmailChange} type="email" placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control onBlur={handlePassword} type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
             <Link to="/logIn">Already Register?</Link>
         </div>
     );
